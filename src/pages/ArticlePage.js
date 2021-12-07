f543d78d7b64d06ea52c4e4fa5c67d41903441b7
Benny Chan <bennyx93@tamu.edu>
2021-12-07 15:19:56 -0600

    Add comment and upvotes feature

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from 'react-router-dom';
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
+import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from '../components/AddCommentForm';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
     const { name }  = useParams();
     const article = articleContent.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/article/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        };
+        fetchData();
+    }, [name]);
 
     if (!article) return (<NotFoundPage />);
 
@@ -15,9 +29,12 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
+        <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo}/>
         {article.content.map((paragraph, key) => (
             <p key={key}>{paragraph}</p>
         ))}
+        <CommentsList comments={articleInfo.comments} />
+        <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
         <h3>Other Articles:</h3>
         <ArticlesList articles={otherArticles} />
         </>
