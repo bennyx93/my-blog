a97a36a9676905cad28120ca41d24ac4008348b4
Benny Chan <bennyx93@tamu.edu>
2021-12-07 08:44:52 -0600

    Implement front-end of blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import NavBar from './NavBar';
+import React, {Component} from 'react';
+import NotFoundPage from './pages/NotFoundPage';
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+            <Routes>
+              <Route exact path='/' element={<HomePage />}/>
+              <Route path='/about' element={<AboutPage />}/>
+              <Route path='/articles-list' element={<ArticlesListPage />}/>
+              <Route path='/article/:name' element={<ArticlePage />}/>
+              <Route path='*' element={<NotFoundPage/>} />
+            </Routes>
+        </div>
+      </div>
+    </Router>
   );
+  }
 }
 
 export default App;
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => (
+    <>
+    <h1>About Me!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet aliquam id diam maecenas ultricies. Arcu cursus euismod quis viverra nibh. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Dolor sit amet consectetur adipiscing. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Lacus sed viverra tellus in. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Elementum pulvinar etiam non quam lacus. A cras semper auctor neque vitae tempus quam.</p>
+    <p>Nibh cras pulvinar mattis nunc sed blandit libero volutpat sed. Sit amet purus gravida quis blandit turpis. Adipiscing commodo elit at imperdiet dui accumsan. Massa id neque aliquam vestibulum. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Quis commodo odio aenean sed adipiscing. Vitae auctor eu augue ut lectus arcu. Consequat semper viverra nam libero. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et.</p>
+    <p>Cras semper auctor neque vitae tempus. Integer feugiat scelerisque varius morbi enim. Vulputate eu scelerisque felis imperdiet proin fermentum. Turpis nunc eget lorem dolor sed. Nibh ipsum consequat nisl vel pretium lectus quam id. Vitae sapien pellentesque habitant morbi tristique. Eleifend donec pretium vulputate sapien nec. Enim lobortis scelerisque fermentum dui faucibus. Faucibus vitae aliquet nec ullamcorper sit. Quis commodo odio aenean sed adipiscing diam donec adipiscing tristique. Sagittis orci a scelerisque purus semper eget duis at. Integer enim neque volutpat ac tincidunt vitae semper quis. Consequat nisl vel pretium lectus quam id leo in vitae. Neque aliquam vestibulum morbi blandit. Habitasse platea dictumst quisque sagittis. Eu scelerisque felis imperdiet proin. Morbi enim nunc faucibus a pellentesque. Vitae tempus quam pellentesque nec nam aliquam. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.</p>
+    </>
+);
+
+export default AboutPage;
